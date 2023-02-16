10bf066c210f1759813db6ef68ce88504e7f5205
SondeOlaniyi <olaminiyisonde@gmail.com>
2023-02-16 07:00:15 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -write a program that uses printf to display output
+ * Return: 0 (completed)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
