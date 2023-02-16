de073b6ba5de5e8e39f7393d28b57f996afceb02
SondeOlaniyi <olaminiyisonde@gmail.com>
2023-02-16 07:21:17 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - C [rogam that prints the size of various types
+ * Return: 0 (complete)
+ */
+int main(void)
+{
+	printf("size of a char: %lu byte(s)", sizeof(char));
+	printf("size of an int: %lu byte(s)", sizeof(int));
+	printf("size of a long int: %lu bytes(s)", sizeof(long int));
+	printf("size of a long long int: %lu bytes(s)", sizeof(long long int));
+	printf("size of a float: %lu bytes(s)", sizeof(float));
+	return (0);
+}
