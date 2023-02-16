ee32bbc3624b8c629ea443c16fd1f74ec79fb661
SondeOlaniyi <olaminiyisonde@gmail.com>
2023-02-16 08:08:33 -0800

    unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - unix is basically a simple operating system
+ * Return: 1 (complete)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
