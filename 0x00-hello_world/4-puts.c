1d022e4d0b5631d6445e1cd02b07fa342121ef57
SondeOlaniyi <olaminiyisonde@gmail.com>
2023-02-16 06:47:09 -0800

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a C programming that prints exactlty the output with a newline
+ * Return:0(completed)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
