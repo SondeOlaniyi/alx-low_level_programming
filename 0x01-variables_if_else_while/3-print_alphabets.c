c3ed1b94bbd7925618690c57a39f85366c59dd42
SondeOlaniyi <olaminiyisonde@gmail.com>
2023-02-17 10:11:52 -0800

    alhabets caps

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -print in capital and smallletter
+ *
+ * Return: 0 (completed)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (i = 'A' ; i <= 'Z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
