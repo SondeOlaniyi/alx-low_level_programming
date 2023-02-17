9190e09fa6e2251d455b7a3d37c63f5bb14b3eef
SondeOlaniyi <olaminiyisonde@gmail.com>
2023-02-17 10:36:05 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - a program that prints single digits
+ *
+ * Return: 0 complete
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
