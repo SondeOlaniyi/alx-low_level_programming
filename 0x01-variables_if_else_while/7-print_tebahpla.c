0b058489809d0421769a79e3e5c71d214b7e2866
SondeOlaniyi <olaminiyisonde@gmail.com>
2023-02-17 10:57:39 -0800

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - write a program that print in reverse
+ *
+ * Return: 0 complete
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i <= 'a' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
