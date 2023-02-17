850e260feed3a0cf5b0928c06e59b6bb44f30b89
SondeOlaniyi <olaminiyisonde@gmail.com>
2023-02-17 14:51:51 -0800

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - a program that prints number in LC
+ *
+ * Return: 0 complete
+ */
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+
+}
