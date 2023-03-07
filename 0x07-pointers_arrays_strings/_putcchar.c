9739ae34c9d6fba0f026c87e6c4f2e7ef4cd6431
SondeOlaniyi <olaminiyisonde@gmail.com>
2023-03-07 03:09:18 -0800

    memset

---
+++ 0x07-pointers_arrays_strings/_putcchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - write the leter c to stdout
+ * @c: The character to print
+ *
+ * Return: always 1
+ * On error, -1 is returned
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+
+}
